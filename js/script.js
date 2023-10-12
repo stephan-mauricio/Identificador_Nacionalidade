3197b20bc36227a1303c057d0b59972cabd7ca12
Stephan Maurício <stephanbenevides@gmail.com>
2023-10-12 16:16:17 +0100

    Actualizacao do codigo
    Olá!
    
    Fiz algumas alterações no código, falta-me corrigir o erro lógico na parte da Validação dos dados e implementar a última funcionalidade para exibir os dados verificados na tela.

--- js/script.js
+++ js/script.js
@@ -1,78 +1,95 @@
-/*Programando ás interações com o utilizador*/
-var NomeCompleto = window.document.getElementById('nomecompleto')
+                                        /*Programando ás interações com o utilizador*/
+/* Objeto de Validação */
+var dados = 
+{
+    NomeCompl: 'Alberto Chico Francisco', 
+    filiacao: 'Delmo Santos Francisco e Sandra Elizabeth Chico',
+    Bilhete_Identidade: '004056103GA041',
+    residencia: '4 de Abril', 
+    bairro: 'Casa Verde',
+    naturalidade: 'Lubango',
+    provincia: 'Huila',
+    Data_Nascimento: '22/03/1997',
+    sexo: 'Masculino',
+    altura: 1.70,
+    Data_Emissao: '10/02/2023',
+    Data_Caducidade: '10/03/2024'
+}
 
-var filiacao = window.document.getElementById('filiacao')
-
-var Bilhete_Identidade = window.document.getElementById('bilhete_identidade')
-
-var residencia = window.document.getElementById('residencia')
-
-var bairro = window.document.getElementById('bairro')
-
-var naturalidade = window.document.getElementById('naturalidade')
-
-var provincia = window.document.getElementById('provincia')
-
-var Data_Nascimento = window.document.getElementById('data_nascimento')
-
-var sexo = window.document.getElementById('sexo')
-
-var altura = window.document.getElementById('altura')
-
-var Data_Emissao = window.document.getElementById('data_emissao')
-
-var Data_Caducidade = window.document.getElementById('data_caducidade')
-
+/* Função de Verificação */
 function verificar() {
-    var nome_processado = window.document.getElementById('nome_processado')
-    var recebe_nome= NomeCompleto.value
-    nome_processado.innerHTML=`<p>${recebe_nome}</p>`
 
-    var filiacao_processado = window.document.getElementById('filiacao_processado')
-    var recebe_filiacao = filiacao.value
-    filiacao_processado.innerHTML = `<p>${recebe_filiacao}</p>`
+    var nome = window.document.getElementById('nomecompleto')
+    var nome_completo = nome.value
 
-    var bilhete_processado = window.document.getElementById('bilhete_processado')
-    var recebe_bilhete = Bilhete_Identidade.value
-    bilhete_processado.innerHTML =  `<p>${recebe_bilhete}</p>`
+    var filiacao = window.document.getElementById('filiacao')
+    var filiacao_completo = filiacao.value
 
-    var residencia_processado = window.document.getElementById('residencia_processado') 
-    var recebe_residencia_processado =  residencia.value
-    residencia_processado.innerHTML = `<p>${recebe_residencia_processado}</p>`
+    var Bilhete_Identidade = window.document.getElementById('bilhete_identidade')
+    var Bilhete_Identidade_completo = Bilhete_Identidade.value
 
-    var bairro_processado = window.document.getElementById('bairro_processado')
-    var recebe_bairro_processado = bairro.value
-    bairro_processado.innerHTML= `<p>${recebe_bairro_processado}</p>`
+    var residencia = window.document.getElementById('residencia')
+    var residencia_completo = residencia.value
 
-    var naturalidade_processado = window.document.getElementById('naturalidade_processado')
-    var recebe_naturalidade_processado= naturalidade.value
-    naturalidade_processado.innerHTML= `<p>${recebe_naturalidade_processado}</p>`
+    var bairro = window.document.getElementById('bairro')
+    var bairro_completo = bairro.value
 
-    var provincia_processado = window.document.getElementById('provincia_processado')
-    var recebe_provincia_processado= provincia.value
-    provincia_processado.innerHTML= `<p>${recebe_provincia_processado}</p>`
+    var naturalidade = window.document.getElementById('naturalidade')
+    var naturalidade_completo = naturalidade.value
 
-    var nascimento_processado = window.document.getElementById('nascimento_processado')
-    var recebe_nascimento_processado= Data_Nascimento.value
-    nascimento_processado.innerHTML= `<p>${recebe_nascimento_processado}</p>`
+    var provincia = window.document.getElementById('provincia')
+    var provincia_completo = provincia.value
 
-    var sexo_processado = window.document.getElementById('sexo_processado')
-    var recebe_sexo_processado= sexo.value
-    sexo_processado.innerHTML= `<p>${recebe_sexo_processado}</p>`
+    var Data_Nascimento = window.document.getElementById('data_nascimento')
+    var Data_Nascimento_completo = Data_Nascimento.value
 
-    var altura_processdo = window.document.getElementById('altura_processdo')
-    var recebe_altura_processado= altura.value
-    altura_processado.innerHTML= `<p>${recebe_altura_processado}</p>`
+    var sexo = window.document.getElementById('sexo')
+    var sexo_completo = sexo.value
 
-    var emitido_processado = window.document.getElementById('emitido_processado')
-    var recebe_emitido_processado= Data_Emissao.value
-    emitido_processado.innerHTML= `<p>${recebe_emitido_processado}</p>`
+    var altura = window.document.getElementById('altura')
+    var altura_completo = altura.value
+
+    var Data_Emissao = window.document.getElementById('data_emissao')
+    var Data_Emissao_completo = Data_Emissao.value
+
+    var Data_Caducidade = window.document.getElementById('data_caducidade')
+    var data_caducidade_completo = Data_Caducidade.value
 
 
-    var data_caducidade_processado = window.document.getElementById('data_caducidade_processado')
-    var recebe_data_caducidade_processado= Data_Caducidade.value
-    data_caducidade_processado.innerHTML= `<p>${recebe_data_caducidade_processado}</p>`
+    /* Validando os Dados */
+    if (
+            dados.NomeCompl == nome_completo || 
+            dados.filiacao == filiacao_completo || 
+            dados.Bilhete_Identidade == Bilhete_Identidade_completo || 
+            dados.residencia == residencia_completo || 
+            dados.bairro == bairro_completo || 
+            dados.naturalidade == naturalidade_completo || 
+            dados.provincia == provincia_completo || 
+            dados.Data_Nascimento == Data_Nascimento_completo || 
+            dados.sexo == sexo_completo || 
+            dados.altura == altura_completo || 
+            dados.Data_Emissao == Data_Emissao_completo || 
+            dados.Data_Caducidade == data_caducidade_completo)
+        {
 
-    var dadosverificados = window.document.getElementById('dadosverificados')
-    dadosverificados.innerHTML='Os seus dados foram verificados, com sucesso!'
-}   
+            window.alert('Dados verificados com sucesso!')
+
+    } else if (
+            dados.NomeCompl != nome_completo && 
+            dados.filiacao != filiacao_completo && 
+            dados.Bilhete_Identidade != Bilhete_Identidade_completo && 
+            dados.residencia != residencia_completo && 
+            dados.bairro != bairro_completo && 
+            dados.naturalidade != naturalidade_completo && 
+            dados.provincia != provincia_completo && 
+            dados.Data_Nascimento != Data_Nascimento_completo && 
+            dados.sexo != sexo_completo && 
+            dados.altura != altura_completo && 
+            dados.Data_Emissao != Data_Emissao_completo && 
+            dados.Data_Caducidade != data_caducidade_completo)
+        {
+            window.alert('[Erro!] Verifique os dados e tente novamente.')
+    }   
+
+                                                /* EM DESENVOLVIMENTO */
+}
